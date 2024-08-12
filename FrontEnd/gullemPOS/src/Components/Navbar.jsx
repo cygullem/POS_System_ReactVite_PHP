4793c293d3bae95eb2cb0fd7c06d4e93a6f5f6c4
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-12 13:06:42 +0800

    UI: Navbar

--- FrontEnd/gullemPOS/src/Components/Navbar.jsx
+++ FrontEnd/gullemPOS/src/Components/Navbar.jsx
@@ -1,9 +1,19 @@
+import profilePicture from '../Images/sziasLogo.png';
+
 const Navbar = () => {
     return (
         <>
             <div className="navbar_container flex justify-between gap-2">
                 <div className="navbar_search border flex-1 p-4"></div>
-                <div className="navbar_userProfile border p-4"></div>
+
+                <div className="navbar_userProfile flex items-center">
+                    <i className="fa-regular fa-bell active:scale-90 text-3xl mr-6"></i>
+                    <img className="w-16 h-16 rounded-full border" src={profilePicture} alt="User" />
+                    <div className="ml-2 flex flex-col justify-center">
+                        <span className="text-xl font-medium cursor-pointer select-none">Cy Gullem</span>
+                        <span className="text-xs font-regular cursor-pointer">Front Cashier</span>
+                    </div>
+                </div>
             </div>
         </>
     );
