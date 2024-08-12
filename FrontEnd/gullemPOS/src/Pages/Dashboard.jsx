89278f7cdf01d65d8993d89893ab3c03fbe01e2f
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-12 13:35:52 +0800

    UI: Dashboard

--- FrontEnd/gullemPOS/src/Pages/Dashboard.jsx
+++ FrontEnd/gullemPOS/src/Pages/Dashboard.jsx
@@ -14,12 +14,12 @@ const Dashboard = () => {
             <div className="flex flex-col flex-1 overflow-hidden">
                 <Navbar />
 
-                <div className="menuContent flex p-4 overflow-auto">
-                    <Categories className="mb-4" />
-                    <div className="flex flex-1">
-                        <Menu className="flex-3 mr-4" />
-                        <Invoice className="flex-1 bg-white p-4 rounded-lg shadow-lg" />
+                <div className="menuContent flex p-4 gap-4 overflow-auto">
+                    <div className="flex flex-col flex-1 border">
+                        <Categories className="mb-4" />
+                        <Menu className="mr-4" />
                     </div>
+                    <Invoice className="flex-1 bg-white p-4 rounded-lg shadow-lg" />
                 </div>
             </div>
         </div>
