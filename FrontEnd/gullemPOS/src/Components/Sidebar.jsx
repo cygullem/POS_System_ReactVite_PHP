3d84acd2f264506aa29163d5d907a46532765bbe
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-12 12:08:43 +0800

    UI: sidebar

--- FrontEnd/gullemPOS/src/Components/Sidebar.jsx
+++ FrontEnd/gullemPOS/src/Components/Sidebar.jsx
@@ -8,12 +8,14 @@ const Sidebar = () => {
             <div className="sidebar_header rounded-xl flex items-center justify-center mb-10">
                 <i class="fa-brands fa-slack"></i>
             </div>
-            <div className="header_body flex-1 border mb-4">
-                <ul className="sidebar_links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+            <div className="header_body flex justify-center items-start flex-1 mb-4">
+                <ul className="sidebar_links flex flex-col gap-2">
+                    <li className='p-2 active:scale-90 hover:bg-blue-600 rounded-lg group'>
+                        <i class="fa-solid fa-chart-line group-hover:text-white"></i>
+                    </li>
+                    <li className='p-2 active:scale-90 hover:bg-blue-600 rounded-lg group'>
+                        <i class="fa-solid fa-file-pen group-hover:text-white"></i>
+                    </li>
                 </ul>
             </div>
             <div className="sidebar_footer flex flex-col gap-4">
