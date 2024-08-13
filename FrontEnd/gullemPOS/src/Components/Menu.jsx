3a88b0d4448f569e73bd77639636f955a2f08c3a
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-13 08:54:14 +0800

    UI: Menu button

--- FrontEnd/gullemPOS/src/Components/Menu.jsx
+++ FrontEnd/gullemPOS/src/Components/Menu.jsx
@@ -2,7 +2,10 @@ const Menu = () => {
     return (
         <>
             <div className="menu_container h-full overflow-auto">
-                <h1 className="text-3xl font-semibold">Breakfast Menu</h1>
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-semibold">Breakfast Menu</h1>
+                    <button className="text-2xl font-medium btn btn-success active:scale-95">Add Menu</button>
+                </div>
                 <div className="menu_items_container grid grid-cols-3 mt-4 gap-4">
                     <div className="menu_items rounded-lg cursor-pointer active:scale-95"></div>
                     <div className="menu_items rounded-lg cursor-pointer active:scale-95"></div>
