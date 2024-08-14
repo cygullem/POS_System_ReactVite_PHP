c42c85a67478406dc8d805911c79c0bce0c13567
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-14 23:47:27 +0800

    changed the file name ang the routing for import

--- FrontEnd/gullemPOS/src/Components/Menu.jsx
+++ FrontEnd/gullemPOS/src/Components/Menu.jsx
@@ -1,11 +1,11 @@
 
 
-const BreakfastMenu = () => {
+const Menu = () => {
     return (
         <>
             <div className="menu_container h-full overflow-auto">
                 <div className="flex items-center justify-between">
-                    <h1 className="text-3xl font-semibold">Breakfast Menu</h1>
+                    <h1 className="text-3xl font-semibold">Menu</h1>
                     <button className="flex items-center p-3 gap-3 text-sm rounded-lg bg-blue-700 active:scale-95 active:opacity-70 group transition duration-200 ease-in-out">
                         <i class="fa-solid fa-utensils text-xl text-white group-hover:rotate-[360deg] transition duration-300 ease-in-out"></i>
                         <span className="text-white select-none">
@@ -29,4 +29,4 @@ const BreakfastMenu = () => {
     );
 }
 
-export default BreakfastMenu;
+export default Menu;
--- FrontEnd/gullemPOS/src/Pages/Dashboard.jsx
+++ FrontEnd/gullemPOS/src/Pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../CSS/main.css';
 import Navbar from '../Components/Navbar';
 import Categories from '../Components/Categories';
-import BreakfastMenu from '../Components/BreakfastMenu';
+import Menu from '../Components/Menu';
 import Invoice from '../Components/Invoice';
 
 const Dashboard = () => {
@@ -14,7 +14,7 @@ const Dashboard = () => {
                 <div className="menuContent h-full flex p-4 gap-4 overflow-auto">
                     <div className="flex flex-col flex-1">
                         <Categories className="mb-4" />
-                        <BreakfastMenu className="mr-4" />
+                        <Menu className="mr-4" />
                     </div>
                     <Invoice className="flex-1 rounded-lg shadow-lg" />
                 </div>
