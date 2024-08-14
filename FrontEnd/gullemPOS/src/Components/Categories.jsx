78fde1c193d1bb45217536a326b9f640e752426b
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-08-14 10:25:30 +0800

    UI: Categories icons

--- FrontEnd/gullemPOS/src/Components/Categories.jsx
+++ FrontEnd/gullemPOS/src/Components/Categories.jsx
@@ -5,7 +5,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-bread-slice group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-bread-slice text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Breakfast</h3>
@@ -15,7 +15,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fas fa-hamburger group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fas fa-hamburger text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Lunch</h3>
@@ -25,7 +25,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-bowl-food group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-bowl-food text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Dinner</h3>
@@ -35,7 +35,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-plate-wheat group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-plate-wheat text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Soup</h3>
@@ -45,7 +45,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-ice-cream group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-ice-cream text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Desserts</h3>
@@ -55,7 +55,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-fish-fins group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-fish-fins text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Side  Dish</h3>
@@ -65,7 +65,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-cheese group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-cheese text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Appetizer</h3>
@@ -75,7 +75,7 @@ const Categories = () => {
 
                 <div className="category_item flex items-center h-20 p-4 rounded-lg hover:bg-blue-700 hover:shadow-lg active:scale-95 group select-none">
                     <div className="icon-container flex justify-center items-center w-12 h-12 bg-white rounded-lg">
-                        <i className="fa-solid fa-champagne-glasses group-hover:text-blue-700 text-2xl"></i>
+                        <i className="fa-solid fa-champagne-glasses text-2xl transition duration-300 ease-in-out group-hover:text-blue-700 group-hover:rotate-[360deg]"></i>
                     </div>
                     <div className="text-container ml-4">
                         <h3 className="text-lg font-semibold group-hover:text-white">Beverages</h3>
